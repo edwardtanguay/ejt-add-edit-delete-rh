88bb0dc94742fe0700fe82c14232e26d4e165807
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-10 06:47:31 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
+import { GermanNounFormRow } from './components/GermanNounFormRow';
+import './App.scss';
+
+function App() {
+	const { state, dispatch } = useContext(AppContext);
+
+	// const handleEditButton = (item) => {
+	// 	item.isEditing = !item.isEditing;
+	// 	setGermanNouns([...germanNouns]);
+	// }
+
+	return (
+		<div className="App">
+			<h1>Site with useContext/useReducer</h1>
+			<p>test: {state.count}</p>
+			<div className="countArea">
+				<button onClick={() => dispatch({ type: 'decreaseCount' })}>
+					-
+				</button>
+				<button onClick={() => dispatch({ type: 'increaseCount' })}>
+					+
+				</button>
+			</div>
+			<hr />
+			<p>There are {state.germanNouns.length} nouns.</p>
+			<div className="germanNounArea">
+				{state.germanNouns.map((item) => {
+					return (
+						<fieldset className="germanNoun" key={item.id}>
+							<legend>ID: {item.id}</legend>
+
+							<GermanNounFormRow
+								item={item}
+								label="Article"
+								variable="article"
+							/>
+
+							<GermanNounFormRow
+								item={item}
+								label="Singular"
+								variable="singular"
+							/>
+
+							<GermanNounFormRow
+								item={item}
+								label="Plural"
+								variable="plural"
+							/>
+
+							<div className="buttonRow">
+								<div className="message">{item.message}</div>
+								<div className="buttonArea">
+									<button
+										onClick={() =>
+											dispatch({
+												type: 'toggleEditStatus',
+												payload: item,
+											})
+										}
+									>
+										Edit
+									</button>
+									<button>Delete</button>
+									<button>Add</button>
+								</div>
+							</div>
+						</fieldset>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/AppContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+export const AppContext = createContext();
+
+const initialState = {
+	count: 0,
+	germanNouns: ['nnn']
+};
+
+function reducer(state, action) {
+	const _state = { ...state };
+	let item = null;
+	let property = null;
+	let value = null;
+	switch (action.type) {
+		case 'increaseCount':
+			_state.count++;
+			break;
+		case 'decreaseCount':
+			_state.count--;
+			break;
+		case 'loadGermanNouns':
+			_state.germanNouns = action.payload;
+			break;
+		case 'toggleEditStatus':
+			item = action.payload;
+			item.isEditing = !item.isEditing;
+			item.message = item.isEditing ? 'Editing item...' : '';
+			break;
+		case 'changeItemRowValue':
+			item = action.payload.item;
+			property = action.payload.property;
+			value = action.payload.value;
+			item[property] = value;
+			break;
+
+	}
+	return _state;
+}
+
+export const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(() => {
+		(async () => {
+			const _germanNouns = ((await axios.get('http://localhost:4555/germanNouns')).data);
+			_germanNouns.forEach(noun => {
+				noun.isEditing = false;
+				noun.message = '';
+			})
+			dispatch({ type: 'loadGermanNouns', payload: _germanNouns });
+		})();
+	}, []);
+
+	return <AppContext.Provider value={{
+		state,
+		dispatch
+	}}>{children}</AppContext.Provider>;
+};
